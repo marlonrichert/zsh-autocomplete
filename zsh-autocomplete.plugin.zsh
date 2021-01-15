1bc62f7859262d02cc3aa3f1472869194a3baa71
Marlon Richert <marlonrichert@users.noreply.github.com>
2021-01-15 19:13:49 +0200

    Integrate better w/ autosuggestions & highlighting
    Interact more gracefully with `zsh-autosuggestions` &
    `zsh-syntax-highlighting`.
    
    Fixes part of issue #170.

--- zsh-autocomplete.plugin.zsh
+++ zsh-autocomplete.plugin.zsh
@@ -3,6 +3,7 @@ setopt NO_singlelinezle
 () {
   emulate -L zsh
   zmodload -Fa zsh/parameter p:history p:funcstack p:functions
+  zmodload -F zsh/zutil b:zstyle
 
   # Workaround for https://github.com/zdharma/zinit/issues/366
   [[ -v functions[.zinit-shade-off] ]] &&
@@ -16,23 +17,6 @@ setopt NO_singlelinezle
 
   typeset -gU FPATH fpath=( ${${(%):-%x}:A:h}/*(/) $fpath[@] )
 
-  # In case we're sourced _after_ `zsh-autosuggestions`
-  builtin autoload -Uz add-zsh-hook
-  add-zsh-hook -d precmd _zsh_autosuggest_start
-
-  # In case we're sourced _before_ `zsh-autosuggestions`
-  builtin autoload -Uz .autocomplete.patch
-  .autocomplete.patch add-zsh-hook
-  add-zsh-hook() {
-    # Prevent `_zsh_autosuggest_start` from being added.
-    local -i ret=0
-    if (( ${@[(I)_zsh_autosuggest_start]} == 0 )); then
-      .autocomplete.add-zsh-hook "$@"; ret=$?
-    fi
-    return 0
-  }
-
-  zmodload -F zsh/zutil b:zstyle
   builtin autoload -Uz .autocomplete.__init__
   .autocomplete.__init__
   local mod; for mod in compinit config widget key key-binding recent-dirs async; do
