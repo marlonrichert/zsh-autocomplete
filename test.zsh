7d2755834f500a4aad15ef8b5db980f2a691c7cc
GabrielDeml <gabrielddeml@gmail.com>
2021-04-14 22:47:11 -0400

    It might work now

--- test.zsh
+++ test.zsh
@@ -1,17 +1,22 @@
 # https://opensource.apple.com/source/zsh/zsh-65/zsh/Doc/help/
+# https://gist.github.com/cinhtau/ea0bed82dfa8f996c3cd4149c2c31672
 
-# local -A just_values
+local -a just_values
 typeset -A values_sorted
 typeset -A history_my
-just_values=(a b c)
-history_my=(d e f)
-print "$s" ${(@)just_values}
+typeset -A values
+# just_values=(a b c)
+history_my=([1]=d [2]=e  [3]=f [4]=e)
+print ${(kv@)history_my}
+# print "$s" ${(@)just_values}
 for key val in "${(@kv)history_my}"; do
-  print $val
+  # print $val
   values[$val]=$key
   just_values+=$val
 done
+
 for val in "${(@vu)just_values}"; do
-  values_sorted[$val]=$key
+  values_sorted[$values[$val]]=$val 
 done
-print "$s" ${(@)just_values}
+print ${(kv@)values_sorted}
+#  print -f '%s:%s' "${(kv@)values_sorted}"
