b164ae7bc70978131c2515bca4cb3bcaf3f95dbc
Marlon Richert <marlonrichert@users.noreply.github.com>
2021-07-04 14:29:48 +0300

    Improve `all-matches` performance

--- run-tests.zsh
+++ run-tests.zsh
@@ -1,4 +1,4 @@
 #!/bin/zsh -f
-print $OSTYPE =zsh $ZSH_VERSION $ZSH_PATCHLEVEL =clitest
+print $VENDOR $OSTYPE =zsh $ZSH_VERSION $ZSH_PATCHLEVEL =clitest
 local +h -a fpath=( ${0:h}/*(-/) $fpath )
 FPATH=$FPATH zsh -f =clitest --list-run --progress dot --prompt '%' -- ${0:h}/.clitest/*.md
