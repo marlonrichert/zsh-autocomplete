0edbecaa09967a4442b0944d290726c4b851637f
Marlon Richert <marlonrichert@users.noreply.github.com>
2023-02-23 09:03:03 +0200

    Improve performance & sorting of history search
    Resolves #459 and #519.

--- .clitest/__init__.zsh
+++ .clitest/__init__.zsh
@@ -1,4 +1,4 @@
 setopt localoptions extendedglob clobber NO_aliases localloops pipefail NO_shortloops NO_unset
 zmodload zsh/param/private
 autoload -Uz zmathfunc && zmathfunc
-builtin autoload -UWz $PWD/functions{,/widget}/.autocomplete.*~*.zwc(N-.:P)
+builtin autoload -UWz $PWD/functions{,/{completion,widget}}/[._]autocomplete.*~*.zwc(DN-.:P)
--- run-tests.zsh
+++ run-tests.zsh
@@ -2,6 +2,6 @@
 cd $( git rev-parse --show-toplevel )
 
 env -i HOME=$( mktemp -d ) PATH=$PATH FPATH=$FPATH zsh -f -- \
-    =clitest  --list-run --progress dot --prompt '%' \
+    =clitest --list-run --progress dot --prompt '%' --color always \
         --pre-flight 'git --version; print $PWD $VENDOR $OSTYPE =zsh $ZSH_VERSION $ZSH_PATCHLEVEL' \
         -- $PWD/.clitest/*.md
