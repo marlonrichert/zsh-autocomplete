784efee23d3ea229e307d2e4075ed7b6d102e574
Marlon Richert <marlonrichert@users.noreply.github.com>
2023-06-07 15:04:59 +0300

    Remove obsolete ZInit workaround

--- zsh-autocomplete.plugin.zsh
+++ zsh-autocomplete.plugin.zsh
@@ -1,14 +1,6 @@
 #!/bin/zsh
 () {
-  zmodload -F zsh/parameter p:funcfiletrace p:functions
-
-  # Workaround for https://github.com/zdharma/zinit/issues/366
-  # NOTE: Needs to come before _everything_ else!
-  [[ -v functions[.zinit-shade-off] ]] &&
-      .zinit-shade-off "${___mode:-load}"
-  [[ -v functions[.zinit-tmp-subst-off] ]] &&
-      .zinit-tmp-subst-off "${___mode:-load}"
-
+  zmodload -F zsh/parameter p:funcfiletrace
   zmodload zsh/param/private
 
   typeset -ga _autocomplete__func_opts=(
