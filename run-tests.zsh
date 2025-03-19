4284c0db07ecae5c371d4dafd8ea50002a6347a7
Marlon Richert <marlonrichert@users.noreply.github.com>
2025-03-19 15:04:09 +0200

    Fix warning in test script

--- run-tests.zsh
+++ run-tests.zsh
@@ -2,7 +2,8 @@
 cd $( git rev-parse --show-toplevel )
 
 git --version
-typeset -p1 PWD VENDOR OSTYPE =zsh ZSH_VERSION ZSH_PATCHLEVEL
+print =zsh
+typeset -p1 PWD VENDOR OSTYPE ZSH_VERSION ZSH_PATCHLEVEL
 env -i HOME=$( mktemp -d ) PATH=$PATH FPATH=$FPATH zsh -f -- \
     =clitest --list-run --progress dot --prompt '%' --color always \
         -- $PWD/Tests/*.md
