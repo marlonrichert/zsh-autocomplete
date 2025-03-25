f5adf1d5e150a98a2b5d92d02ce5de244bc4d705
Marlon Richert <marlonrichert@users.noreply.github.com>
2025-03-25 08:37:49 +0200

    Make `run-tests.zsh` work in VS Code

--- run-tests.zsh
+++ run-tests.zsh
@@ -3,7 +3,8 @@ cd $( git rev-parse --show-toplevel )
 
 git --version
 print =zsh
-typeset -p1 PWD VENDOR OSTYPE ZSH_VERSION ZSH_PATCHLEVEL
+typeset -p1 VENDOR OSTYPE ZSH_VERSION ZSH_PATCHLEVEL
+
 env -i HOME=$( mktemp -d ) PATH=$PATH FPATH=$FPATH zsh -f -- \
-    =clitest --list-run --progress dot --prompt '%' --color always \
+    clitest/clitest --list-run --progress dot --prompt '%' --color always \
         -- $PWD/Tests/*.md
